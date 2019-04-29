b9fe1dabda79210ea83f16317ca660f8aca8edcd
UncleLev <risentean@gmail.com>
2019-04-29 21:04:01 +0300

    adding

--- js/modul_7/js/index.js
+++ js/modul_7/js/index.js
@@ -1,1 +1,72 @@
-'use strict';
+/*
+  1. Модифицируйте готовую функцию createPostCard() из задания 
+    номер 6 (https://codepen.io/goit-fe-adv/pen/MVPaeZ) так, 
+    чтобы она принимала объект post с данными для заполнения полей 
+    в карточке.
+      
+  2. Создайте функцию createCards(posts), которая принимает массив
+    объектов-карточек, вызывает функцию createPostCard(post) столько
+    раз, сколько объектов в массиве, сохраняя общий результат и возвращает 
+    массив DOM-элементов всех постов.
+    
+  3. Повесьте все посты в какой-то уже существующий DOM-узел.
+*/
+const post = {
+    img: "https://placeimg.com/400/150/arch",
+    title: "Post title text",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: 'link-0.com'
+};
+
+const posts = [{
+        img: "https://placeimg.com/400/150/arch",
+        title: "Post title 1",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-1.com'
+    },
+    {
+        img: "https://placeimg.com/400/150/nature",
+        title: "Post title 2",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-2.com'
+    },
+    {
+        img: "https://placeimg.com/400/150/arch",
+        title: "Post title 3",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-3.com'
+    }
+];
+
+createPostCard(post);
+
+function createPostCard(post) {
+    let body = document.querySelector('.post');
+
+    let wrap = document.createElement('div');
+    let img = document.createElement('img');
+    let title = document.createElement('h2');
+    let text = document.createElement('p');
+    let link = document.createElement('a');
+
+    img.setAttribute('src', post.img);
+    text.textContent = post.text;
+    link.setAttribute('href', post.link);
+    title.textContent = post.title;
+
+    wrap.append(img);
+    wrap.append(title);
+    wrap.append(text);
+    wrap.append(link);
+
+    body.append(wrap);
+};
+
+createCards(posts);
+
+function createCards(posts) {
+
+    for (const itm of posts) {
+        createPostCard(itm);
+    }
+};
